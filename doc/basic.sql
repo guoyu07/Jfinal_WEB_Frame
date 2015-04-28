ad8a76b1e906dc7dd354b04bff9a57fa9a7852d4
唐选东 <tangxuandong@zhubajie.com>
2015-04-28 13:51:16 +0800

    basic

--- doc/basic.sql
+++ doc/basic.sql
@@ -16,6 +16,36 @@ CREATE DATABASE IF NOT EXISTS `basic` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `basic`;
 
 
+-- 导出  表 basic.sys_button 结构
+DROP TABLE IF EXISTS `sys_button`;
+CREATE TABLE IF NOT EXISTS `sys_button` (
+  `id` int(6) NOT NULL AUTO_INCREMENT COMMENT '功能Id',
+  `menuId` int(3) NOT NULL COMMENT '菜单Id',
+  `name` varchar(32) NOT NULL COMMENT '显示名称',
+  `value` varchar(32) NOT NULL COMMENT '功能值',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8 COMMENT='功能按钮';
+
+-- 正在导出表  basic.sys_button 的数据：~5 rows (大约)
+DELETE FROM `sys_button`;
+/*!40000 ALTER TABLE `sys_button` DISABLE KEYS */;
+INSERT INTO `sys_button` (`id`, `menuId`, `name`, `value`) VALUES
+	(20, 14, '添加', 'MenuController.addview'),
+	(21, 14, '修改', 'MenuController.editview'),
+	(22, 14, '修改状态', 'MenuController.editstatus'),
+	(23, 14, '删除', 'MenuController.delview'),
+	(24, 13, '删除', 'UserController.delete'),
+	(25, 13, '添加', 'UserController.addview'),
+	(26, 13, '修改', 'UserController.editview'),
+	(27, 13, '修改状态', 'UserController.upstatus'),
+	(28, 12, 'RoleController.permissionview', 'RoleController.permissionview'),
+	(29, 12, '添加', 'RoleController.addview'),
+	(30, 12, '修改', 'RoleController.editview'),
+	(31, 12, '修改状态', 'RoleController.editstatus'),
+	(32, 12, '删除', 'RoleController.delview');
+/*!40000 ALTER TABLE `sys_button` ENABLE KEYS */;
+
+
 -- 导出  表 basic.sys_button_log 结构
 DROP TABLE IF EXISTS `sys_button_log`;
 CREATE TABLE IF NOT EXISTS `sys_button_log` (
@@ -41,16 +71,16 @@ DELETE FROM `sys_button_log`;
 DROP TABLE IF EXISTS `sys_login_log`;
 CREATE TABLE IF NOT EXISTS `sys_login_log` (
   `id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT '日志ID',
-  `userId` bigint(11) DEFAULT NULL COMMENT '用户ID',
-  `loginTime` bigint(13) DEFAULT NULL COMMENT '登录时间',
+  `userId` bigint(11) NOT NULL COMMENT '用户ID',
+  `loginTime` bigint(13) NOT NULL COMMENT '登录时间',
   `logoutTime` bigint(13) DEFAULT NULL COMMENT '退出时间',
-  `logIp` varchar(31) DEFAULT NULL COMMENT '登录Ip',
+  `logIp` varchar(32) NOT NULL COMMENT '登录Ip',
   `lastlogTime` bigint(13) DEFAULT NULL COMMENT '上次登录时间',
-  `status` int(1) DEFAULT NULL COMMENT '状态;1在线0下线',
+  `status` int(1) NOT NULL DEFAULT '1' COMMENT '状态;1在线0下线',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COMMENT='登录日志表';
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8 COMMENT='登录日志表';
 
--- 正在导出表  basic.sys_login_log 的数据：~9 rows (大约)
+-- 正在导出表  basic.sys_login_log 的数据：~13 rows (大约)
 DELETE FROM `sys_login_log`;
 /*!40000 ALTER TABLE `sys_login_log` DISABLE KEYS */;
 INSERT INTO `sys_login_log` (`id`, `userId`, `loginTime`, `logoutTime`, `logIp`, `lastlogTime`, `status`) VALUES
@@ -61,13 +91,20 @@ INSERT INTO `sys_login_log` (`id`, `userId`, `loginTime`, `logoutTime`, `logIp`,
 	(12, -1, 1429769367034, NULL, '0:0:0:0:0:0:0:1', 1429758893362, 1),
 	(13, -1, 1429777412607, NULL, '0:0:0:0:0:0:0:1', 1429769367034, 1),
 	(14, -1, 1429784229250, NULL, '0:0:0:0:0:0:0:1', 1429777412607, 1),
-	(15, -1, 1429793543573, NULL, '0:0:0:0:0:0:0:1', 1429784229250, 1),
-	(16, -1, 1429795257817, NULL, '0:0:0:0:0:0:0:1', 1429793543573, 1),
-	(17, -1, 1429795496867, NULL, '0:0:0:0:0:0:0:1', 1429795257817, 1),
-	(18, -1, 1429795511540, NULL, '0:0:0:0:0:0:0:1', 1429795496867, 1),
-	(19, -1, 1429795526930, NULL, '0:0:0:0:0:0:0:1', 1429795511540, 1),
-	(20, -1, 1429795568699, NULL, '0:0:0:0:0:0:0:1', 1429795526930, 1),
-	(21, -1, 1429795761712, NULL, '0:0:0:0:0:0:0:1', 1429795568699, 1);
+	(15, -1, 1430105249463, NULL, '0:0:0:0:0:0:0:1', 1429784229250, 1),
+	(16, -1, 1430191392739, NULL, '0:0:0:0:0:0:0:1', 1430105249463, 1),
+	(17, -1, 1430191634606, NULL, '0:0:0:0:0:0:0:1', 1430191392739, 1),
+	(18, -1, 1430192084427, NULL, '0:0:0:0:0:0:0:1', 1430191634606, 1),
+	(19, -1, 1430196863494, NULL, '0:0:0:0:0:0:0:1', 1430192084427, 1),
+	(20, -1, 1430196920733, NULL, '0:0:0:0:0:0:0:1', 1430196863494, 1),
+	(21, -1, 1430198433233, NULL, '0:0:0:0:0:0:0:1', 1430196920733, 1),
+	(22, -1, 1430198570384, NULL, '0:0:0:0:0:0:0:1', 1430198433233, 1),
+	(23, -1, 1430198800545, NULL, '0:0:0:0:0:0:0:1', 1430198570384, 1),
+	(24, -1, 1430198896866, NULL, '0:0:0:0:0:0:0:1', 1430198800545, 1),
+	(25, -1, 1430199285822, NULL, '0:0:0:0:0:0:0:1', 1430198896866, 1),
+	(26, -1, 1430199423581, NULL, '0:0:0:0:0:0:0:1', 1430199285822, 1),
+	(27, -1, 1430199841740, NULL, '0:0:0:0:0:0:0:1', 1430199423581, 1),
+	(28, -1, 1430199895707, NULL, '0:0:0:0:0:0:0:1', 1430199841740, 1);
 /*!40000 ALTER TABLE `sys_login_log` ENABLE KEYS */;
 
 
@@ -75,18 +112,18 @@ INSERT INTO `sys_login_log` (`id`, `userId`, `loginTime`, `logoutTime`, `logIp`,
 DROP TABLE IF EXISTS `sys_menu`;
 CREATE TABLE IF NOT EXISTS `sys_menu` (
   `id` int(3) NOT NULL AUTO_INCREMENT COMMENT '菜单ID',
-  `name` varchar(20) DEFAULT NULL COMMENT '名称',
+  `name` varchar(20) NOT NULL COMMENT '名称',
   `type` int(4) DEFAULT NULL COMMENT '类型',
   `href` varchar(64) DEFAULT NULL COMMENT 'url地址',
   `icon` varchar(64) DEFAULT NULL COMMENT '图标',
   `description` varchar(64) DEFAULT NULL COMMENT '描述',
   `target` varchar(20) DEFAULT NULL COMMENT '目标（mainFrame、 _blank、_self、_parent、_top）',
   `rel` varchar(128) DEFAULT NULL COMMENT 'rel',
-  `parentId` int(3) DEFAULT NULL COMMENT '父级',
-  `createTime` bigint(13) DEFAULT NULL COMMENT '创建时间',
+  `parentId` int(3) NOT NULL DEFAULT '-1' COMMENT '父级',
+  `createTime` bigint(13) NOT NULL COMMENT '创建时间',
   `isUsed` int(1) NOT NULL DEFAULT '1' COMMENT '是否有效',
   `sortNo` int(8) DEFAULT NULL COMMENT '排序号',
-  `permission` varchar(255) DEFAULT NULL COMMENT '权限值',
+  `permission` varchar(64) DEFAULT NULL COMMENT '菜单权限值',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COMMENT='菜单表';
 
@@ -95,9 +132,9 @@ DELETE FROM `sys_menu`;
 /*!40000 ALTER TABLE `sys_menu` DISABLE KEYS */;
 INSERT INTO `sys_menu` (`id`, `name`, `type`, `href`, `icon`, `description`, `target`, `rel`, `parentId`, `createTime`, `isUsed`, `sortNo`, `permission`) VALUES
 	(11, '系统管理', 0, '', '', '', '', '', -1, 1407140785011, 1, 99, '1'),
-	(12, '角色列表', 0, '/sys/role/', 'am-icon-file ', '角色列表', 'navTab', 'user_role_listview', 11, 1407141914856, 1, 1, 'add_edit_delete'),
-	(13, '用户列表', 0, '/sys/user/', NULL, '用户列表', 'navTab', 'user_info_listview', 11, 1407141964456, 1, 2, 'add_edit_delete'),
-	(14, '菜单列表', 0, '/sys/menu/', NULL, '菜单列表', 'navTab', 'order_info_listview', 11, 1407142023060, 1, 3, 'MenuController_MenuController.addview_MenuController.editview_MenuController.editstatus_MenuController.delview'),
+	(12, '角色列表', 0, '/sys/role/', 'am-icon-file        ', '角色列表', 'navTab', 'user_role_listview', 11, 1407141914856, 0, 2, 'RoleController'),
+	(13, '用户列表', 0, '/sys/user/', 'am-icon-hand-o-up    ', '用户列表', 'navTab', 'user_info_listview', 11, 1407141964456, 1, 1, 'UserController'),
+	(14, '菜单列表', 0, '/sys/menu/', 'am-icon-file ', '菜单列表', 'navTab', 'order_info_listview', 11, 1407142023060, 1, 3, 'MenuController'),
 	(15, '日志管理', 0, '', '', '', '', '', -1, 1409642077554, 1, 100, '1'),
 	(16, '操作日志', 0, '/sys/log/button', ' ', NULL, 'navTab', 'log_node_listview', 15, 1409642132551, 1, 1, 'add_edit_delete'),
 	(17, '登录日志', NULL, '/sys/log/login', ' ', '登录日志', 'navTab', 'log_login_listview', 15, 1409648279391, 1, 1, 'add_edit_delete_save'),
@@ -105,47 +142,14 @@ INSERT INTO `sys_menu` (`id`, `name`, `type`, `href`, `icon`, `description`, `ta
 /*!40000 ALTER TABLE `sys_menu` ENABLE KEYS */;
 
 
--- 导出  表 basic.sys_permission 结构
-DROP TABLE IF EXISTS `sys_permission`;
-CREATE TABLE IF NOT EXISTS `sys_permission` (
-  `id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT '权限管理表ID',
-  `permission` varchar(64) DEFAULT NULL COMMENT '权限值',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=146 DEFAULT CHARSET=utf8 COMMENT='权限管理表';
-
--- 正在导出表  basic.sys_permission 的数据：~18 rows (大约)
-DELETE FROM `sys_permission`;
-/*!40000 ALTER TABLE `sys_permission` DISABLE KEYS */;
-INSERT INTO `sys_permission` (`id`, `permission`) VALUES
-	(19, 'add_edit_delete'),
-	(20, 'add_edit_delete'),
-	(22, 'add_edit_delete'),
-	(23, 'add_edit_delete'),
-	(132, NULL),
-	(133, 'add_edit_delete'),
-	(134, 'add_edit_delete'),
-	(135, 'add_edit_delete'),
-	(136, NULL),
-	(137, 'com.twosnail.basic.constant.permethod.role.LogPermission'),
-	(138, 'com.twosnail.basic.constant.permethod.role.LoginPermission'),
-	(139, NULL),
-	(140, 'addview'),
-	(141, 'com.twosnail.basic.constant.permethod.role.UserPermission'),
-	(142, 'com.twosnail.basic.constant.permethod.role.MenuPermission'),
-	(143, NULL),
-	(144, 'com.twosnail.basic.constant.permethod.role.LogPermission'),
-	(145, 'com.twosnail.basic.constant.permethod.role.LoginPermission');
-/*!40000 ALTER TABLE `sys_permission` ENABLE KEYS */;
-
-
 -- 导出  表 basic.sys_role 结构
 DROP TABLE IF EXISTS `sys_role`;
 CREATE TABLE IF NOT EXISTS `sys_role` (
   `id` int(4) NOT NULL AUTO_INCREMENT COMMENT '角色ID',
   `roleCode` varchar(32) DEFAULT NULL COMMENT '角色编号',
-  `roleName` varchar(64) NOT NULL COMMENT '角色名称',
+  `roleName` varchar(32) NOT NULL COMMENT '角色名称',
   `isUsed` int(1) NOT NULL DEFAULT '1' COMMENT '是否有效',
-  `createTime` bigint(13) DEFAULT NULL COMMENT '创建时间',
+  `createTime` bigint(13) NOT NULL COMMENT '创建时间',
   `parentId` int(4) NOT NULL COMMENT '父级角色',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COMMENT='角色表';
@@ -154,35 +158,42 @@ CREATE TABLE IF NOT EXISTS `sys_role` (
 DELETE FROM `sys_role`;
 /*!40000 ALTER TABLE `sys_role` DISABLE KEYS */;
 INSERT INTO `sys_role` (`id`, `roleCode`, `roleName`, `isUsed`, `createTime`, `parentId`) VALUES
-	(1, 'root', 'root', 1, NULL, -1),
-	(2, 'admin', 'admin', 1, NULL, 1),
-	(3, '12', '12', 1, 1429680662074, -1),
-	(4, '13', '13', 1, 1429681603916, -1),
-	(5, '14', '14', 1, 1429682907199, -1),
-	(6, '5', '15', 1, 1429683810758, -1),
-	(7, '16', '16', 1, 1429683814657, -1);
+	(1, 'root', 'root', 1, 1429680662074, -1),
+	(2, 'admin', 'admin', 0, 1429680662074, 1),
+	(4, '13', '13', 1, 1429681603916, -1);
 /*!40000 ALTER TABLE `sys_role` ENABLE KEYS */;
 
 
 -- 导出  表 basic.sys_role_permission 结构
 DROP TABLE IF EXISTS `sys_role_permission`;
 CREATE TABLE IF NOT EXISTS `sys_role_permission` (
-  `id` int(8) NOT NULL AUTO_INCREMENT,
-  `roleId` int(8) NOT NULL,
-  `permission` varchar(50) NOT NULL,
+  `id` int(8) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `roleId` int(4) NOT NULL COMMENT '角色Id',
+  `permission` varchar(64) NOT NULL COMMENT '权限值（菜单和功能）',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COMMENT='角色权限';
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8 COMMENT='角色权限';
 
--- 正在导出表  basic.sys_role_permission 的数据：~1 rows (大约)
+-- 正在导出表  basic.sys_role_permission 的数据：~8 rows (大约)
 DELETE FROM `sys_role_permission`;
 /*!40000 ALTER TABLE `sys_role_permission` DISABLE KEYS */;
 INSERT INTO `sys_role_permission` (`id`, `roleId`, `permission`) VALUES
-	(1, 1, '140'),
-	(3, 2, 'MenuController'),
-	(4, 2, 'MenuController.addview'),
-	(5, 2, 'MenuController.addview'),
-	(6, 2, 'MenuController.addview'),
-	(7, 2, 'MenuController.addview');
+	(16, 1, '1'),
+	(17, 1, 'RoleController'),
+	(18, 1, 'RoleController.permissionview'),
+	(19, 1, 'RoleController.addview'),
+	(20, 1, 'RoleController.editview'),
+	(21, 1, 'RoleController.editstatus'),
+	(22, 1, 'RoleController.delview'),
+	(23, 1, 'UserController'),
+	(24, 1, 'UserController.delete'),
+	(25, 1, 'UserController.addview'),
+	(26, 1, 'UserController.editview'),
+	(27, 1, 'UserController.upstatus'),
+	(28, 1, 'MenuController'),
+	(29, 1, 'MenuController.addview'),
+	(30, 1, 'MenuController.editview'),
+	(31, 1, 'MenuController.editstatus'),
+	(32, 1, 'MenuController.delview');
 /*!40000 ALTER TABLE `sys_role_permission` ENABLE KEYS */;
 
 
@@ -216,21 +227,7 @@ DELETE FROM `sys_user`;
 /*!40000 ALTER TABLE `sys_user` DISABLE KEYS */;
 INSERT INTO `sys_user` (`id`, `roleId`, `userName`, `passWord`, `createTime`, `sex`, `birthday`, `idCard`, `mobile`, `phone`, `email`, `addr`, `createId`, `createIp`, `operateId`, `opetateTime`, `isDefPassWord`, `isUsed`, `sortNo`) VALUES
 	(9, 1, 'root', '14e1b600b1fd579f47433b88e8d85291', 1406691374696, 1, NULL, NULL, 0, NULL, NULL, NULL, -1, '0:0:0:0:0:0:0:1', 9, 1429494461796, 1, 1, 1),
-	(10, 2, 'admin', '14e1b600b1fd579f47433b88e8d85291', 1406691444637, 1, NULL, '', 15826008618, NULL, '', '', -1, '0:0:0:0:0:0:0:1', 9, 1428396852183, 1, 1, 1),
-	(11, 4, 'user01', '123456', 1428391501309, 0, '2015-01-02', '50023519941002927X', 15826008619, NULL, 'user01@qq.com', '重庆市云阳县', 9, '0:0:0:0:0:0:0:1', 9, 1428484824882, 1, 0, 15),
 	(12, 1, 'user03', '123456', 1428396925659, 1, NULL, '', 15826008617, NULL, '', '', 9, '0:0:0:0:0:0:0:1', 9, 1428397051036, 1, 1, 1),
-	(23, 4, '31', '1', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 1, NULL),
-	(25, 4, '33', '1', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 1, NULL),
-	(26, 4, '34', '1', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 1, NULL),
-	(27, 4, '35', '1', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 1, NULL),
-	(28, 4, '36', '1', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 1, NULL),
-	(29, 4, '37', '1', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 1, NULL),
-	(30, 4, '38', '1', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 1, NULL),
-	(31, 4, '40', '1', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 1, NULL),
-	(32, 4, '41', '1', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 1, NULL),
-	(33, 4, '42', '1', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 1, NULL),
-	(34, 4, '43', '1', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 1, NULL),
-	(35, 4, '44', '1', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 1, NULL),
 	(36, 4, '45', '1', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 1, NULL),
 	(37, 4, '46', '1', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 1, NULL),
 	(38, 4, '47', '1', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 1, NULL);
